f4ce59eba219e5dbe4dd3d5a600a0a328f75f014
Magyari Sandor Szilard <sancyx@gmail.com>
2018-10-24 19:05:52 +0200

    fix 2

--- zeppelin-plugins/launcher/spark-on-k8s/src/main/java/org/apache/zeppelin/interpreter/launcher/SparkK8sInterpreterLauncher.java
+++ zeppelin-plugins/launcher/spark-on-k8s/src/main/java/org/apache/zeppelin/interpreter/launcher/SparkK8sInterpreterLauncher.java
@@ -86,8 +86,12 @@ public class SparkK8sInterpreterLauncher extends SparkInterpreterLauncher {
       env.put("ZEPPELIN_SPARK_K8_CLUSTER", "true");
       env.put("ZEPPELIN_INTP_JAVA_OPTS", "-Dlog4j" +
         ".configuration=/opt/zeppelin/conf/log4j_k8_cluster.properties");
-      env.put("ZEPPELIN_SPARK_CONF", "--conf spark.executor.extraJavaOptions=\"-Dlog4j" +
+
+      StringBuilder sparkConfBuilder = new StringBuilder(env.get("ZEPPELIN_SPARK_CONF"));
+      sparkConfBuilder.append("--conf spark.executor" +
+        ".extraJavaOptions=\"-Dlog4j" +
         ".configuration=/opt/zeppelin/conf/log4j_k8_cluster.properties\"");
+      env.put("ZEPPELIN_SPARK_CONF", sparkConfBuilder.toString());
 
 
       return new SparkK8sRemoteInterpreterManagedProcess(
